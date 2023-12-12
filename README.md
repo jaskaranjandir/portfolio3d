3762732334afd83f7ff4b75fd8440cce14ce3ad3
Jaskaran Singh <jaskaranjandir17@gmail.com>
2023-12-12 17:29:40 +0530

    Portfolio progress saved

---
+++ README.md
@@ -0,0 +1,7 @@
+# Build and Deploy an Amazing 3D Developer Portfolio in React with Three.js
+
+<h1>Steps to run the project</h1>
+<ol>
+    <li>npm install</li>
+    <li>Npm run dev</li>
+</ol>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 bg-white z-20 flex justify-center items-center rounded-lg font-bold"
+      >
+        <p className="blue-gradient_text">JS</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
+          Projects
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
